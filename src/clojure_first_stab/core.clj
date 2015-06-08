5441a02eb1a534709c9be3cede0bcf0d786f0d29
nerdgrass <arboerstler@gmail.com>
2015-06-07 21:52:02 -0400

    first commit, lein new app

---
+++ src/clojure_first_stab/core.clj
@@ -0,0 +1,7 @@
+(ns clojure-first-stab.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/clojure_first_stab/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-first-stab.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-first-stab.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
